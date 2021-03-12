422e1fbf1aab1a28cc090c1d41076c66595381b3
Emily Haines <haineseb@g.cofc.edu>
2021-03-11 23:17:39 -0500

    its late and i have an 8am so im stopping

--- app/src/main/java/com/example/tipcalculator_haines/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator_haines/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.tipcalculator.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //input for amount
+    //input for people splitting check
+    //radio buttons for preset tip amounts
+    //last radio button for custom tip
+    //button to reset vals
+    //button to calculate tip
+    //  ^^^sends user to activity 2, only displays a tip ammt summ and button to go back
+    //amounts (tip, total, total per person) all textview
+    //retain through rotation
+    //no hardcoded strings or dimensions in .java
+    //pull from like strings.xml and shit
+    //error message if bill < $1, total people < 1, tip % < 1
 }
